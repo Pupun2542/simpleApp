2e77507ede8f8978eec866ceb5c05e9be610214c
Pupun2542 <ppupun2542@gmail.com>
2022-10-05 12:17:32 +0700

    init

---
+++ hooks/useName.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const useName = (defaultvalue) => {
+    const [value, setValue] = useState(defaultvalue);
+    const apiPath = "/api"
+    const valuePath = apiPath.concat("/value");
+
+    const getValue = async()=> {
+        try {
+            const res = await axios.get(valuePath);
+            if (res.status === 200) {
+                console.log(res.data)
+                setValue(res.data);
+            }
+            
+        } catch (err) {
+            alert(err)
+        }
+    }
+    const updateValue = async (e)=> {
+        e.preventDefault()
+        try {
+            const res = await axios.post(valuePath, {
+                name: e.target.name.value
+            });
+            if (res.status === 200) {
+                alert("update data successful")
+            }
+        } catch(err) {
+            alert(err)
+        }
+    }
+  return {
+    getValue,
+    updateValue,
+    value
+  }
+}
+
+export default useName
---
+++ pages/api/value.js
@@ -0,0 +1,40 @@
+import sqlite3 from 'sqlite3'
+import { open } from "sqlite";
+// const db = new sqlite3.Database("test.db");
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const db = await open({
+        filename: "test.db",
+        driver: sqlite3.Database,
+      });
+      
+      const item = await db.get("select * from test where id=1");
+      if (item) {
+        // console.log(`UPDATE test SET name = '${req.body.name}' WHERE id = 1`)
+        db.exec(`UPDATE test SET name = '${req.body.name}' WHERE id = 1`);
+      } else {
+        console.log("insert")
+        db.exec(`insert into test (name) values ('${req.body.name}')`);
+      }
+      return res.status(200).send("insert item sucessful");
+    } catch (err) {
+      res.status(400).send("error: "+err);
+      return;
+    }
+  } else if (req.method === "GET") {
+    try {
+        const db = await open({
+            filename: "test.db",
+            driver: sqlite3.Database,
+          });
+          const item = await db.get("select * from test where id=1");
+          return res.status(200).json(item.name)
+    } catch (err) {
+        return res.status(400).json("error: "+ err)
+    }
+  } else {
+    res.status(500).send("cannot " + req.method);
+  }
+}
